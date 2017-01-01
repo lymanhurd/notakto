f71231335c19bbf33dfdc88a119e28095dc79e3b
Lyman Hurd <lyman.hurd@gmail.com>
2017-01-01 09:21:04 +0000

    Fixed imports.

--- bottle_app.py
+++ bottle_app.py
@@ -4,9 +4,11 @@
 # The Secrets of Notakto: Winning at X-only  Tic-Tac-Toe by Thane E. Plambeck,
 # Greg Whitehead.
 from bottle import default_app, route, static_file
+from notakto import smart_move
+
 
 @route('/move/<move_string>')
 def computer_move(move_string):
-    return notakto.smart_move(move_string)
+    return smart_move(move_string)
 
 application = default_app()
