04e648a00cbfb691aa9431eebd11372c904aee41
Lyman Hurd <lyman.hurd@gmail.com>
2017-02-12 01:48:40 +0000

    Updated bottle.py

--- bottle_app.py
+++ bottle_app.py
@@ -18,7 +18,7 @@ def computer_move(move_string):
 # A simplistic checkers AI by Lyman Hurd.  Expects a 32 character string.  This
 # endpoint means it is black's move.
 @route('/checkers/black/<move_string>')
-def computer_move(move_string):
+def black_computer_move(move_string):
     """Return move for checkers."""
     return find_move(move_string, True)
 
@@ -27,7 +27,7 @@ def computer_move(move_string):
 # the first character being the player whose move it is.  This
 # endpoint means it is red's move.
 @route('/checkers/red/<move_string>')
-def computer_move(move_string):
+def red_computer_move(move_string):
     """Return move for checkers."""
     return find_move(move_string, False)
 
