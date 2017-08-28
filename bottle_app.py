e3538062a776bf57d3e9227affd72e7e2ba970da
Lyman Hurd <lyman.hurd@gmail.com>
2017-08-28 16:01:39 +0000

    Bringing repository up to date with site.

--- bottle_app.py
+++ bottle_app.py
@@ -1,8 +1,8 @@
 """Web app for hurd-sullivan.com."""
 
-from bottle import default_app, hook, response, route
+from bottle import default_app, hook, response, route, request, template
 from notakto.notakto import smart_move
-from checkers_ai.tree_search import find_move
+# from checkers_ai.tree_search import find_move
 
 @hook('after_request')
 def enable_cors():
@@ -22,19 +22,24 @@ def computer_move(move_string):
 # Checkers
 # A simplistic checkers AI by Lyman Hurd.  Expects a 32 character string.  This
 # endpoint means it is black's move.
-@route('/checkers/black/<move_string>')
-def black_computer_move(move_string):
-    """Return move for checkers."""
-    return find_move(move_string, True)
+#@route('/checkers/black/<move_string>')
+#def black_computer_move(move_string):
+#    """Return move for checkers."""
+#    return find_move(move_string, True)
 
 # Checkers
 # A simplistic checkers AI by Lyman Hurd.  Expects a 32 character string with
 # the first character being the player whose move it is.  This
 # endpoint means it is red's move.
-@route('/checkers/red/<move_string>')
-def red_computer_move(move_string):
-    """Return move for checkers."""
-    return find_move(move_string, False)
+#@route('/checkers/red/<move_string>')
+#def red_computer_move(move_string):
+#    """Return move for checkers."""
+#    return find_move(move_string, False)
 
+@route('/forum')
+def display_forum():
+    forum_id = request.query.id
+    page = request.query.page or '1'
+    return template('Forum ID: {{id}} (page {{page}})', id=forum_id, page=page)
 
 application = default_app()
