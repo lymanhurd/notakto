a0ab4777ea9d48304b4bd92177674892bd214a81
Lyman Hurd <lyman.hurd@gmail.com>
2019-01-08 22:45:16 -0800

    Fixed unit tests.

--- cribbage/cribbage_test.py
+++ cribbage/cribbage_test.py
@@ -40,5 +40,5 @@ class TestCribbage(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     unittest.main()
