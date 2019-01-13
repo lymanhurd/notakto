36ef09e8314a04afbde56eb97674dab702241b29
Lyman Hurd <lyman.hurd@gmail.com>
2019-01-13 11:51:39 -0800

    Added logic for games.

--- cribbage/dojo.py
+++ cribbage/dojo.py
@@ -13,17 +13,16 @@ from score import score, score_sequence
 
 # actually we end up playng twice this many
 _NUM_GAMES = 1
-_NUM_HANDS = 1
+_NUM_HANDS = 10000
 
 
-def play_games(player1, player2, num_games):
+def play_games(player1, player2, num_games=1):
     # The stronger (new) player is assumed to go second so we record wins/margin
     # from Player 2's point of view.
     wins, margin = 0, 0
-    deck = Deck()
     for i in range(num_games):
         logging.debug('Game %d', i)
-        game_win, game_margin = play_game(deck, player1, player2)
+        game_win, game_margin = play_game(player1, player2)
         wins += game_win
         margin += game_margin
         logging.debug('(%d, %d)', wins, i - wins)
@@ -53,7 +52,8 @@ def play_hands(player1, player2, num_hands):
     return margin
 
 
-def play_game(deck, dealer, pone):
+def play_game(dealer, pone):
+    deck = Deck()
     dscore, pscore = 0, 0
     while dscore < 121 and pscore < 121:
         deck.shuffle()
@@ -170,5 +170,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     player1 = create_player(level=0)
     player2 = create_player(level=1)
-    margin = play_hands(player1, player2, _NUM_HANDS)
-    print('Average margin ', margin/(2*_NUM_HANDS))
+#    margin = play_hands(player1, player2, _NUM_HANDS)
+#    print('Average margin ', margin/(2*_NUM_HANDS))
+    wins, margin = play_games(player1, player2)
+    print('Average wins {} margin {}'.format(wins, margin))
